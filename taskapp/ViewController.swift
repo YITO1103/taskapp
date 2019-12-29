fdcd086187084537e8f7e721a7d8934695074675
YITO1103 <yuta.j.1103@gmail.com>
2019-12-29 13:08:06 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by user1 on 2019/12/29.
+//  Copyright © 2019 yutaka.ito4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
